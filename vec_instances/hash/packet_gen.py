f069af4ed81405def4ce4b4cae559ce06ce74208
HamidrezaSK <shrsanaee@gmail.com>
2023-08-16 15:30:38 +0100

    Add hash usecase

---
+++ vec_instances/hash/packet_gen.py
@@ -0,0 +1,7 @@
+from scapy.all import IPv6,raw
+p = IPv6(src='2001:db8:85a3::8a2e:370:7334', dst='2001:db8:85a3::98ab:370:7334')
+a = p.show(dump=True)
+print (a)
+
+with open('ipv6_in.mem', 'wb') as f:
+    f.write(raw(p))
