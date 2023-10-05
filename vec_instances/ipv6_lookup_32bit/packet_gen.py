97c09a0b0e329c6f3c3bf74e3b22d03d388b7e5a
HamidrezaSK <shrsanaee@gmail.com>
2023-10-05 17:17:55 +0100

    Update packet gen script to create multiple random ipv6 packets

--- vec_instances/ipv6_lookup_32bit/packet_gen.py
+++ vec_instances/ipv6_lookup_32bit/packet_gen.py
@@ -1,7 +1,16 @@
 from scapy.all import IPv6,raw
-p = IPv6(src='2a03:2880:f21a:0083:face:b00b:0360:25de', dst='2001:db8:85a3::98ab:370:7334')
-a = p.show(dump=True)
-print (a)
+import random
+import string
 
-with open('ipv6_in.mem', 'wb') as f:
-    f.write(raw(p))
+def generate_random_ip():
+    result = ""
+    for i in range(8):
+        section = ''.join(random.choice('a' + 'b' + 'c' + 'c' + 'd' + 'e' + 'f' + string.digits) for _ in range(4))
+        result += section + ':'
+    return result[:-1]
+
+if __name__ == "__main__":
+    with open('ipv6_in.mem', 'wb') as f:
+        for i in range(32):
+            p = IPv6(src=generate_random_ip(), dst=generate_random_ip())
+            f.write(raw(p))
