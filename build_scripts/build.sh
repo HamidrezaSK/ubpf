0926570c4a7c509a3590d3b771710bcd587a3286
HamidrezaSK <shrsanaee@gmail.com>
2023-10-15 20:07:33 +0100

    Add build and run scripts for test

---
+++ build_scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y llvm clang python3-pip libboost-all-dev libbpf-dev cmake cpufrequtils libelf-dev libpcap-dev build-essential linux-tools-common linux-tools-generic tcpdump libc6-dev-i386 htop
+cd ..
+
+# Change directory to vec_instances/eBPF/compare/bpf
+cd vec_instances/eBPF/compare/bpf
+
+# Run the Python script
+python packet_gen_random.py
+
+# Change back to the previous directory
+cd -
+
+# Compile eBPF programs
+clang -O2 -target bpf -c vec_instances/eBPF/compare/bpf/xdp_prog_32.c -o vec_instances/eBPF/compare/bpf/xdp_prog_32.o
+clang -O2 -target bpf -c vec_instances/eBPF/compare/bpf/xdp_prog_16.c -o vec_instances/eBPF/compare/bpf/xdp_prog_16.o
+clang -O2 -target bpf -c vec_instances/eBPF/compare/bpf/xdp_prog_8.c -o vec_instances/eBPF/compare/bpf/xdp_prog_8.o
+
+# Change directory to vec_instances/eBPF/compare/x86
+cd vec_instances/eBPF/compare/x86
+
+# Run the Python script
+python packet_gen_random.py
+
+# Compile x86 programs
+clang -g -O2 xdp_prog_16.c -o xdp_prog_16
+clang -g -O2 xdp_prog_32.c -o xdp_prog_32
+clang -g -O2 xdp_prog_8.c -o xdp_prog_8
---
+++ build_scripts/run.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Set CPU governor to performance for CPU core 9
+sudo cpufreq-set -c 9 -g performance
+
+# Change directory
+cd ../
+
+# Define the commands to run
+commands=(
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_32.o -d -j -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_16.o -d -j -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_8.o -d -j -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+)
+
+# Log file
+log_file="log_vanila_jit.log"
+
+# Loop to run the commands 5 times and log their return values
+for i in {1..5}; do
+  for command in "${commands[@]}"; do
+    echo "Running: $command"
+    $command >> "vec_instances/eBPF/compare/bpf/$log_file" 2>&1
+    echo "Exit code: $?" >> "$log_file"
+    echo "--------------------" >> "$log_file"
+  done
+done
+
+
+commands=(
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_32.o -v -d -j -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_16.o -v -d -j -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_8.o -v -d -j -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+)
+
+# Log file
+log_file="log_modified_jit.log"
+
+# Loop to run the commands 5 times and log their return values
+for i in {1..5}; do
+  for command in "${commands[@]}"; do
+    echo "Running: $command"
+    $command >> "vec_instances/eBPF/compare/bpf/$log_file" 2>&1
+    echo "Exit code: $?" >> "$log_file"
+    echo "--------------------" >> "$log_file"
+  done
+done
+
+commands=(
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_32.o -d -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_16.o -d -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+  "taskset 0x200 build/bin/ubpf_test vec_instances/eBPF/compare/bpf/xdp_prog_8.o -d -m vec_instances/eBPF/compare/bpf/ipv6_in.mem"
+)
+
+# Log file
+log_file="log_aot.log"
+
+# Loop to run the commands 5 times and log their return values
+for i in {1..5}; do
+  for command in "${commands[@]}"; do
+    echo "Running: $command"
+    $command >> "vec_instances/eBPF/compare/bpf/$log_file" 2>&1
+    echo "Exit code: $?" >> "$log_file"
+    echo "--------------------" >> "$log_file"
+  done
+done
+
+cd vec_instances/eBPF/compare/x86
+
+commands=(
+  "./xdp_prog_8"
+  "./xdp_prog_16"
+  "./xdp_prog_32"
+)
+
+# Log file
+log_file="log.log"
+
+# Loop to run the commands 5 times and log their return values
+for i in {1..5}; do
+  for command in "${commands[@]}"; do
+    echo "Running: $command"
+    $command >> "vec_instances/eBPF/compare/x86/$log_file" 2>&1
+    echo "Exit code: $?" >> "$log_file"
+    echo "--------------------" >> "$log_file"
+  done
+done
