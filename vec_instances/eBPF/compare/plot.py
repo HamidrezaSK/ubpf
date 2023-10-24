959c83f7d30e0a6e6590a925309bb790a05d39a3
HamidrezaSK <shrsanaee@gmail.com>
2023-10-24 10:54:39 +0100

    Change plot from line to bar chart

--- vec_instances/eBPF/compare/plot.py
+++ vec_instances/eBPF/compare/plot.py
@@ -1,48 +1,60 @@
 import re
 import matplotlib.pyplot as plt
 from statistics import mean 
+import numpy as np
 
 def plot(vanila_jit, modified_jit, aot, x86, cache):
     keys = list(vanila_jit.keys())
+    bar_width = 0.2  # Adjust the bar width as needed
+
     fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
-    # Scatter plot for each mode
-    for mode, data in [("aot times", aot), ("vanilla_jit times", vanila_jit), ("x86 times", x86), ("modified_jit times", modified_jit)]:
-        x_values = keys
-        y_values =  [mean(data[key]) for key in keys]
-        axs[0].plot(x_values, y_values, label=mode, marker='o')
-        y_values =  [min(data[key]) for key in keys]
-        axs[1].plot(x_values, y_values, label=mode, marker='o')
-        y_values =  [max(data[key]) for key in keys]
-        axs[2].plot(x_values, y_values, label=mode, marker='o')
+    x_values = np.arange(len(keys))
 
+    mean_base = np.array([mean(vanila_jit[key]) for key in keys])
+    min_base = np.array([mean(vanila_jit[key]) for key in keys])
+    max_base = np.array([mean(vanila_jit[key]) for key in keys])
+
+
+
+    # Bar chart for each mode
+    for i, (mode, data) in enumerate([ ("vanilla_jit times", vanila_jit), ("x86 times", x86), ("modified_jit times", modified_jit)]):
+        y_values = np.array([mean(data[key]) for key in keys])
+        axs[0].bar(x_values + i * bar_width, y_values/mean_base, bar_width, label=mode)
+        y_values = np.array([min(data[key]) for key in keys])
+        axs[1].bar(x_values + i * bar_width, y_values/min_base, bar_width, label=mode)
+        y_values = np.array([max(data[key]) for key in keys])
+
+        axs[2].bar(x_values + i * bar_width, y_values/max_base, bar_width, label=mode)
 
     # Customize the plot
     axs[0].set_xlabel('Rules Number')
-    axs[0].set_ylabel('Execution Time')
+    axs[0].set_ylabel('Execution Time/Vanilla Execution Time')
     axs[0].set_title('Mean Execution Times for Different Modes ' + cache)
-    axs[0].set_xticks(keys)
+    axs[0].set_xticks(x_values + bar_width * (len(keys) - 1) / 2)
+    axs[0].set_xticklabels(keys)
     axs[0].legend()
     axs[0].grid(True)
 
     axs[1].set_xlabel('Rules Number')
-    axs[1].set_ylabel('Execution Time')
+    axs[1].set_ylabel('Execution Time/Vanilla Execution Time')
     axs[1].set_title('Min Execution Times for Different Modes ' + cache)
-    axs[1].set_xticks(keys)
+    axs[1].set_xticks(x_values + bar_width * (len(keys) - 1) / 2)
+    axs[1].set_xticklabels(keys)
     axs[1].legend()
     axs[1].grid(True)
 
     axs[2].set_xlabel('Rules Number')
-    axs[2].set_ylabel('Execution Time')
+    axs[2].set_ylabel('Execution Time/Vanilla Execution Time')
     axs[2].set_title('Max Execution Times for Different Modes ' + cache)
-    axs[2].set_xticks(keys)
+    axs[2].set_xticks(x_values + bar_width * (len(keys) - 1) / 2)
+    axs[2].set_xticklabels(keys)
     axs[2].legend()
     axs[2].grid(True)
 
     # Show the plot
     plt.tight_layout()
     plt.show()
-
 
 def extract_times(path):
     with open(path) as my_file:
