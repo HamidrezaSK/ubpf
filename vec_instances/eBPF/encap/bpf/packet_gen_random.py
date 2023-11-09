5e56a719aa346ebcd8afcdec5751f1983df0d83e
HamidrezaSK <shrsanaee@gmail.com>
2023-11-09 18:43:33 +0000

    Change the input process of packet_gen_random from input to argv

--- vec_instances/eBPF/encap/bpf/packet_gen_random.py
+++ vec_instances/eBPF/encap/bpf/packet_gen_random.py
@@ -1,6 +1,7 @@
 from scapy.all import IPv6,raw,Ether
 import random
 import string
+import sys
 
 def generate_random_ip():
     result = ""
@@ -10,9 +11,18 @@ def generate_random_ip():
     return result[:-1]
 
 if __name__ == "__main__":
-    count = input("how many packets do you want in a batch: ")
-    with open('ipv6_in_'+count+'.mem', 'wb') as f:
-        for i in range(int(count)):
+    count = 0
+    if len(sys.argv) < 2:
+        count = 1
+    else:
+        try:
+            count = int(sys.argv[1])
+        except ValueError:
+            print("Error: Invalid integer provided.")
+            sys.exit(1)
+
+    with open('ipv6_in_'+str(count)+'.mem', 'wb') as f:
+        for i in range(count):
             p = IPv6(src=generate_random_ip(), dst=generate_random_ip())
             eth_frame = Ether(dst="00:11:22:33:44:55", src="a1:b2:c3:d4:e5:f6")
             packet = eth_frame / p
