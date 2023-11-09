9e86ba7f2fb2941d25b5b7770c94be5aa9853d74
HamidrezaSK <shrsanaee@gmail.com>
2023-11-09 16:15:58 +0000

    Add the multiple packet ability to packet script (eth and ip header)

--- vec_instances/eBPF/encap/x86/packet_gen_random.py
+++ vec_instances/eBPF/encap/x86/packet_gen_random.py
@@ -1,4 +1,4 @@
-from scapy.all import IPv6,raw
+from scapy.all import IPv6,raw,Ether
 import random
 import string
 
@@ -10,7 +10,10 @@ def generate_random_ip():
     return result[:-1]
 
 if __name__ == "__main__":
-    with open('ipv6_in.mem', 'wb') as f:
-        for i in range(4):
+    count = input("how many packets do you want in a batch: ")
+    with open('ipv6_in_'+count+'.mem', 'wb') as f:
+        for i in range(int(count)):
             p = IPv6(src=generate_random_ip(), dst=generate_random_ip())
-            f.write(raw(p))
+            eth_frame = Ether(dst="00:11:22:33:44:55", src="a1:b2:c3:d4:e5:f6")
+            packet = eth_frame / p
+            f.write(raw(packet))
